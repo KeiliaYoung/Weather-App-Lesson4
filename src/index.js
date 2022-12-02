c15ca22c12c04224ae91314e1b84684383188449
KeiliaYoung <39070883+KeiliaYoung@users.noreply.github.com>
2022-12-02 04:46:29 -0500

    Edited index.js position apiKey and apiUrl

--- src/index.js
+++ src/index.js
@@ -12,18 +12,18 @@ let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let day = days[now.getDay()];
 
 let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
 ];
 let month = months[now.getMonth()];
 
@@ -32,43 +32,44 @@ h3.innerHTML = `${day}, ${month} ${date}, ${year} ${hour}:${minutes}`;
 // Search Engine
 
 function displayWeather(response) {
-    console.log(response);
-    let temperatureElement = document.querySelector("#current-temp");
-    let cityElement = document.querySelector("#city");
-    let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind");
-    let iconElement = document.querySelector("#icon");
+  console.log(response);
+  let temperatureElement = document.querySelector("#current-temp");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
 
 
-    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-    cityElement.innerHTML = response.data.city;
-    humidityElement.innerHTML = response.data.temperature.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    iconElement.setAttribute("src", `${response.data.condition.icon_url}`);
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute("src", `${response.data.condition.icon_url}`);
 
 }
 
 function showCity(event) {
-    event.preventDefault();
-    let city = document.querySelector("#city-search-input").value;
-    searchCity(city);
+  event.preventDefault();
+  let city = document.querySelector("#city-search-input").value;
+  searchCity(city);
 }
 
 function searchCity(city) {
-    let apiKey = "ebb9dt4f3c1fda5064cb77ffeebaaf7o";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-    axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
+  let apiKey = "ebb9dt4f3c1fda5064cb77ffeebaaf7o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
 }
 
 function showPosition(position) {
-    let apiKey = "ebb9dt4f3c1fda5064cb77ffeebaaf7o";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${position.data.coord.longitude}&lat=${position.data.coord.latitude}&key=${apiKey}`;
-
-    axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "28483fb0bac69b11e99890f72d1b1c8f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
 }
 
 function getCurrentLocation(event) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+  navigator.geolocation.getCurrentPosition(showPosition);
 }
 
 let enterCity = document.querySelector("#input-city");
@@ -77,29 +78,29 @@ enterCity.addEventListener("submit", showCity);
 let currentButton = document.querySelector("#showPosition");
 currentButton.addEventListener("click", getCurrentLocation);
 
-// F / C
-function search(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#city-search-input");
-    let h1 = document.querySelector("h1");
-    h1.innerHTML = searchInput.value;
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = searchInput.value;
 }
+
 let form = document.querySelector("#input-city");
-form.addEventListener("submit", search);
+form.addEventListener("submit", handleSubmit);
 
 function displayFahrenheitTemperature(event) {
-    event.preventDefault();
-    let currentCelsiusTemperature = document.querySelector("#celsius");
-    currentCelsiusTemperature.innerHTML = "67°C";
+  event.preventDefault();
+  let currentCelsiusTemperature = document.querySelector("#celsius");
+  currentCelsiusTemperature.innerHTML = "67°C";
 }
 
 let fahrenheit = document.querySelector("#fahrenheit");
 fahrenheit.addEventListener("click", displayFahrenheitTemperature);
 
 function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    let currentCelsiusTemperature = document.querySelector("#fahrenheit");
-    currentCelsiusTemperature.innerHTML = "19°F";
+  event.preventDefault();
+  let currentCelsiusTemperature = document.querySelector("#fahrenheit");
+  currentCelsiusTemperature.innerHTML = "19°F";
 }
 
 let celsius = document.querySelector("#celsius");
